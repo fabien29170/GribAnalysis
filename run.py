656f89911d8a2e8de5ace877252a2258a6ffc8e6
Fapu <fapu@pop-os.localdomain>
2021-10-11 21:57:10 +0200

    evol run.py

--- run.py
+++ run.py
@@ -1,17 +1,38 @@
 import pygrib
+import datetime
 import numpy
-grbs = pygrib.open('../.xygrib/grib/test.grb2')
+import math
+grbs = pygrib.open('../../.xygrib/grib/test.grb2')
 
+grbComposanteUWind = grbs.select(name='10 metre U wind component')
+grbComposanteVWind = grbs.select(name='10 metre V wind component')
+NbrPasdeTemps=len(grbComposanteUWind)
+PasdeTemps=datetime.timedelta(hours=1)
 
-grb = grbs.select(name='10 metre U wind component')
-PasdeTemps=len(grb)
-
-g=grb[0]
-print("Composante",g.name," Pas de temps = ",PasdeTemps, " Date=",g.analDate)
-
-tab=g.data()
-for j in range(len(tab[0])):
-    for i in range(len(tab[0][0])):
-        print(tab[1][j][i],' ',tab[2][j][i],' ',tab[0][j][i])
-    print("-------------")
-
+gU=grbComposanteUWind [0]
+print("Composante",gU.name," Pas de temps = ",NbrPasdeTemps, " Date=",gU.analDate)
+gDate=gU.analDate
+for t in range(NbrPasdeTemps):
+    gU=grbComposanteUWind[t]
+    gV=grbComposanteVWind[t]
+    tabU=gU.data()
+    tabV=gV.data()
+    for j in range(len(tabU[0])):
+        for i in range(len(tabU[0][0])):
+            longitude = tabU[2][j][i]
+            if longitude >= 180:
+                longitude = longitude - 360
+                U=tabU[0][j][i]
+                V=tabV[0][j][i]
+                chaineOutput=str(tabU[1][j][i])+','+str(longitude)+',U='+str(U)+','+gDate.isoformat()
+                chaineOutput.replace(" ","")
+                print(chaineOutput)
+                chaineOutput=str(tabV[1][j][i])+','+str(longitude)+',V='+str(V)+','+gDate.isoformat()
+                chaineOutput.replace(" ","")
+                print(chaineOutput)
+                Angle=numpy.arctan(U/V)/math.pi*180
+                print ('Angle',Angle)
+                Vitesse=math.sqrt(U*U+V*V)*3600/1852
+                print ('Vitesse',Vitesse)
+    gDate=gU.analDate+datetime.timedelta(hours=t)
+    
